a8ac0154226fc9248fb58b159f92c23625fcc82d
MabioKlayneAlves <mabiokleyne5@gmail.com>
2024-09-14 00:24:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('yes-button').addEventListener('click', function() {
+    document.getElementById('response-message').textContent = 'Desde o momento em que você disse "sim", meu coração está transbordando de felicidade 💖. Não vejo a hora de viver essa nova jornada ao seu lado, compartilhando momentos, sorrisos e sonhos. Você faz meu mundo mais bonito 🌟. Obrigado(a) por aceitar embarcar nessa aventura comigo. Mal posso esperar para descobrir tudo o que o futuro nos reserva juntos! 😍';
+});
+
+document.getElementById('god forbid-button').addEventListener('click', function() {
+    document.getElementById('response-message').textContent = 'Eu realmente tinha esperanças quando abri meu coração pra você, porque achei que poderíamos construir algo bonito juntos. Sei que cada pessoa tem o direito de seguir seu próprio caminho e fazer suas escolhas, mas não posso negar que ouvir um "não" como resposta ao meu pedido de namoro me deixou arrasado. Eu só queria compartilhar momentos, risos e até as dificuldades com você, mas entendo que nem sempre os sentimentos são recíprocos. Vou respeitar o seu espaço e os seus sentimentos, mas isso não torna essa dor menos real. Vou me afastar um pouco, mas saiba que sempre vou torcer pela sua felicidade, mesmo que não seja ao meu lado.';
+});
+
+document.getElementById('no-button').addEventListener('click', function() {
+    document.getElementById('response-message').textContent = 'Mesmo que as coisas não tenham saído como eu esperava 😔, quero que saiba que meus sentimentos por você são sinceros 💔. Entendo e respeito sua decisão, mas não posso negar que meu coração está um pouco mais pesado agora. Mesmo assim, desejo que você encontre toda a felicidade e amor que merece, mesmo que não seja ao meu lado 💫.';
+});
